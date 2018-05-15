3a445dab13e5edf2452d358d3aca27dcc46ba80c
nTamura <nictamura@gmail.com>
2018-05-14 22:13:31 -0700

    revise test

--- test/test-server.js
+++ test/test-server.js
@@ -16,17 +16,21 @@ describe('Test:', function() {
 
   describe("stream.pipe()", function() {
     const home = process.cwd()
-    it('should output a file to /output', function() {
+    it('should output a file to /output', function(done) {
       const stream = fs.createReadStream(filePath)
       const output = fs.createWriteStream(`${home}/output/${filename}`)
       stream.pipe(output)
       expect(fs.existsSync(`${home}/output/${filename}`)).to.be.true
+      done()
     })
-    it('should have the same filesize', function() {
-      const file1 = fs.statSync(`${home}/${filePath}`).size.toString();
-      const file2 = fs.statSync(`${home}/output/${filename}`).size.toString();
-      expect(file1).to.equal(file2)
-    })
+    it('should have the same filesize', function(done) {
+      setTimeout(function() {
+        const file1 = fs.statSync(`${home}/${filePath}`).size
+        const file2 = fs.statSync(`${home}/output/${filename}`).size
+        expect(file1).to.be.equal(file2)
+        done();
+      }, 50);
+    });
   })
 
   describe("Upon invoking the log flag", function() {
@@ -35,5 +39,5 @@ describe('Test:', function() {
       expect(fs.existsSync(`${home}/logs/logfile.txt`)).to.be.true
     })
   })
-  
+
 });
